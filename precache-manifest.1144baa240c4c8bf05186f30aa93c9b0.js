e3de69727d46ae0c08e66193efa18f98db088355
lusta.vm <lusta.vlad2001@gmail.com>
2020-09-26 16:35:31 +0300

    deploy

---
+++ precache-manifest.1144baa240c4c8bf05186f30aa93c9b0.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dc42c6ef966abebb9c3",
+    "url": "/Matreshka/css/about.76874681.css"
+  },
+  {
+    "revision": "a86e08e4aca78fd81203",
+    "url": "/Matreshka/css/app.4efed78d.css"
+  },
+  {
+    "revision": "e7bcf6e0e53d3a0606db",
+    "url": "/Matreshka/css/chunk-28d77816.14b4e466.css"
+  },
+  {
+    "revision": "becf834c227c54a5ce97",
+    "url": "/Matreshka/css/chunk-2d2bb58c.76874681.css"
+  },
+  {
+    "revision": "0efe0896508a4eb3d215",
+    "url": "/Matreshka/css/chunk-f88b53c8.85e964b6.css"
+  },
+  {
+    "revision": "0c743c4c6bd976ecbd17",
+    "url": "/Matreshka/css/chunk-vendors.433426e8.css"
+  },
+  {
+    "revision": "32df9602d79ee26407f9fa0ba0e3aa6d",
+    "url": "/Matreshka/index.html"
+  },
+  {
+    "revision": "2dc42c6ef966abebb9c3",
+    "url": "/Matreshka/js/about.a1005b1f.js"
+  },
+  {
+    "revision": "a86e08e4aca78fd81203",
+    "url": "/Matreshka/js/app.b52a2f69.js"
+  },
+  {
+    "revision": "e7bcf6e0e53d3a0606db",
+    "url": "/Matreshka/js/chunk-28d77816.e0ecf4bb.js"
+  },
+  {
+    "revision": "f625a74365ffa7bf91f0",
+    "url": "/Matreshka/js/chunk-2d0d3ff4.c9677143.js"
+  },
+  {
+    "revision": "576d6155705878cb44fe",
+    "url": "/Matreshka/js/chunk-2d238255.c165690c.js"
+  },
+  {
+    "revision": "becf834c227c54a5ce97",
+    "url": "/Matreshka/js/chunk-2d2bb58c.4e15ae8d.js"
+  },
+  {
+    "revision": "0efe0896508a4eb3d215",
+    "url": "/Matreshka/js/chunk-f88b53c8.b7132877.js"
+  },
+  {
+    "revision": "0c743c4c6bd976ecbd17",
+    "url": "/Matreshka/js/chunk-vendors.e66c1d8f.js"
+  },
+  {
+    "revision": "833d8182528a24c31b14f9eb0af10af2",
+    "url": "/Matreshka/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Matreshka/robots.txt"
+  }
+]);
---
+++ precache-manifest.82d002d05d5662b987416fc2738a6584.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dc42c6ef966abebb9c3",
+    "url": "/FullStack-Kanban/css/about.76874681.css"
+  },
+  {
+    "revision": "c5bff01e76f1ce323920",
+    "url": "/FullStack-Kanban/css/app.4efed78d.css"
+  },
+  {
+    "revision": "e7bcf6e0e53d3a0606db",
+    "url": "/FullStack-Kanban/css/chunk-28d77816.14b4e466.css"
+  },
+  {
+    "revision": "becf834c227c54a5ce97",
+    "url": "/FullStack-Kanban/css/chunk-2d2bb58c.76874681.css"
+  },
+  {
+    "revision": "0efe0896508a4eb3d215",
+    "url": "/FullStack-Kanban/css/chunk-f88b53c8.85e964b6.css"
+  },
+  {
+    "revision": "0c743c4c6bd976ecbd17",
+    "url": "/FullStack-Kanban/css/chunk-vendors.433426e8.css"
+  },
+  {
+    "revision": "f71619f3480df0ae2dd9281ea438bcd9",
+    "url": "/FullStack-Kanban/index.html"
+  },
+  {
+    "revision": "2dc42c6ef966abebb9c3",
+    "url": "/FullStack-Kanban/js/about.a1005b1f.js"
+  },
+  {
+    "revision": "c5bff01e76f1ce323920",
+    "url": "/FullStack-Kanban/js/app.389106a7.js"
+  },
+  {
+    "revision": "e7bcf6e0e53d3a0606db",
+    "url": "/FullStack-Kanban/js/chunk-28d77816.e0ecf4bb.js"
+  },
+  {
+    "revision": "f625a74365ffa7bf91f0",
+    "url": "/FullStack-Kanban/js/chunk-2d0d3ff4.c9677143.js"
+  },
+  {
+    "revision": "576d6155705878cb44fe",
+    "url": "/FullStack-Kanban/js/chunk-2d238255.c165690c.js"
+  },
+  {
+    "revision": "becf834c227c54a5ce97",
+    "url": "/FullStack-Kanban/js/chunk-2d2bb58c.4e15ae8d.js"
+  },
+  {
+    "revision": "0efe0896508a4eb3d215",
+    "url": "/FullStack-Kanban/js/chunk-f88b53c8.b7132877.js"
+  },
+  {
+    "revision": "0c743c4c6bd976ecbd17",
+    "url": "/FullStack-Kanban/js/chunk-vendors.e66c1d8f.js"
+  },
+  {
+    "revision": "833d8182528a24c31b14f9eb0af10af2",
+    "url": "/FullStack-Kanban/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/FullStack-Kanban/robots.txt"
+  }
+]);
